b65ced364a8fd60dbab73694dc4497f611ff89d9
joshuajung01 <tojoshuajung@gmail.com>
2020-09-27 12:27:14 -0500

    removed token mb

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-token = "NzU5ODA5Njk3NjY1OTA4NzU3.X3C57A.3eNlO8gdfuWBiE8aYFH1EN6GKlg"#os.environ["NzU5ODA5Njk3NjY1OTA4NzU3.X3C57A.3eNlO8gdfuWBiE8aYFH1EN6GKlg"]
+token = os.environ["DISCORD_TOKEN"]
 bot = commands.Bot(command_prefix="!")
 
 
